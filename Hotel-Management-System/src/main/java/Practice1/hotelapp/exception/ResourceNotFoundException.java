5fe91685088f74108baa032e44a257cdfd823708
Dj <djssss1234@gmail.com>
2026-02-16 22:13:19 +0530

    Removed unnecessary nested folder

--- Hotel-Management-System/src/main/java/Practice1/hotelapp/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package Practice1.hotelapp.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Hotel-Management-System/src/main/java/Practice1/hotelapp/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package Practice1.hotelapp.repository;
-
-import Practice1.hotelapp.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking,Integer> {
-}
